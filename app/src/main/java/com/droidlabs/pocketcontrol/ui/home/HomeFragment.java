9776fdb197aadcc294f8f17e99a9177914a3e0d4
Savio Jojo Thalakkotoor <savio.thalakkotoor@st.ovgu.de>
2020-05-24 08:44:01 +0200

    Revert "implemented home screen"
    This reverts commit dd5a5623af01990cefac9bbd71b5385126b51fa3

--- app/src/main/java/com/droidlabs/pocketcontrol/ui/home/HomeFragment.java
+++ app/src/main/java/com/droidlabs/pocketcontrol/ui/home/HomeFragment.java
@@ -4,63 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-
-import android.widget.TextView;
-
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.droidlabs.pocketcontrol.R;
-import com.droidlabs.pocketcontrol.db.PocketControlDB;
-import com.droidlabs.pocketcontrol.db.transaction.Transaction;
-import com.droidlabs.pocketcontrol.db.transaction.TransactionDao;
-
-import java.util.List;
 
 
 public class HomeFragment extends Fragment {
-
-    private PocketControlDB db = PocketControlDB.getDatabase(getContext());
-    private Animation topAnimation;
-    private TextView textViewAmount, textViewExpense, textViewIncome;
-    private TransactionDao transactionDao;
-    private Float totalAmount = 0f;
-    private Float totalExpense = 0f;
-    private Float totalIncome = 0f;
-    private List<Transaction> allTransactions;
-
     @Nullable
     @Override
     public final View onCreateView(
             final LayoutInflater inf, final @Nullable ViewGroup container, final @Nullable Bundle savedInstanceState) {
-        View view = inf.inflate(R.layout.fragment_home, container, false);
-
-        textViewAmount = view.findViewById(R.id.homeScreenTop);
-        textViewIncome = view.findViewById(R.id.homeScreenTotalIncome);
-        textViewExpense = view.findViewById(R.id.homeScreenTotalExpense);
-
-        topAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.top_animation);
-        textViewAmount.setAnimation(topAnimation);
-
-        transactionDao = db.transactionDao();
-        allTransactions = transactionDao.getAllTransactions();
-
-        if (!allTransactions.isEmpty()) {
-            for (Transaction transaction: allTransactions) {
-                if (transaction.getType().equals(1)) {
-                    totalExpense += transaction.getAmount();
-                } else {
-                    totalIncome += transaction.getAmount();
-                }
-            }
-        }
-        totalAmount =  totalIncome - totalExpense;
-
-        textViewExpense.setText("Totatl Expense : $ " + totalExpense.toString());
-        textViewIncome.setText("Totatl Income   : $ " + totalIncome.toString());
-        textViewAmount.setText("$ " + totalAmount.toString());
-
-        return view;
+        return inf.inflate(R.layout.fragment_home, container, false);
     }
 }
