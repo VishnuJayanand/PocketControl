5fc02ca96a5625a4406b0d85976f2f9e3745979a
Lauro F. Müller <lauro.muller@lfmuller.com>
2020-05-28 15:46:18 +0200

    add bottom sheet + small adjustments to design of category screen

--- app/src/main/java/com/droidlabs/pocketcontrol/ui/transaction/TransactionFragment.java
+++ app/src/main/java/com/droidlabs/pocketcontrol/ui/transaction/TransactionFragment.java
@@ -1,21 +1,10 @@
 package com.droidlabs.pocketcontrol.ui.transaction;
 
-import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Spinner;
-
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
@@ -28,25 +17,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.droidlabs.pocketcontrol.R;
-import com.droidlabs.pocketcontrol.db.category.Category;
 import com.droidlabs.pocketcontrol.db.transaction.Transaction;
 import com.droidlabs.pocketcontrol.ui.categories.CategoryViewModel;
-import com.droidlabs.pocketcontrol.utils.DateUtils;
 
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 
 public class TransactionFragment extends Fragment implements TransactionListAdapter.OnTransactionNoteListener {
 
     private TransactionViewModel transactionViewModel;
-    private CategoryViewModel categoryViewModel;
     private TransactionListAdapter transactionListAdapter;
-    private EditText editTextToDate, editTextFromDate;
-    private Button imageAmountButton;
-    private EditText editTextToAmount, editTextFromAmount;
-    private final Calendar fromDate = Calendar.getInstance(), toDate = Calendar.getInstance();
+    private FilterBottomSheetDialog filterBottomSheetDialog;
 
     @Override
     public final View onCreateView(
@@ -58,7 +39,8 @@ public class TransactionFragment extends Fragment implements TransactionListAdap
         RecyclerView recyclerView = view.findViewById(R.id.transactionListView);
 
         transactionViewModel = new ViewModelProvider(this).get(TransactionViewModel.class);
-        categoryViewModel = new ViewModelProvider(this).get(CategoryViewModel.class);
+        CategoryViewModel categoryViewModel = new ViewModelProvider(this).get(CategoryViewModel.class);
+
         transactionListAdapter = new TransactionListAdapter(getActivity(), this, transactionViewModel);
 
         recyclerView.setAdapter(transactionListAdapter);
@@ -71,7 +53,21 @@ public class TransactionFragment extends Fragment implements TransactionListAdap
             }
         });
 
+        transactionViewModel.setCategoryFilter(false, "");
+        transactionViewModel.setAmountFilter(false, Float.MIN_VALUE, Float.MAX_VALUE);
+        transactionViewModel.setDateFilter(false, Long.MIN_VALUE, Long.MAX_VALUE);
+
+        filterBottomSheetDialog = new FilterBottomSheetDialog(transactionViewModel, categoryViewModel);
+
         AppCompatButton addTransactionLayout = view.findViewById(R.id.addTransactionButton);
+
+        Button expandFilterButton = view.findViewById(R.id.expandFilterButton);
+        expandFilterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                filterBottomSheetDialog.show(getParentFragmentManager(), "Tag");
+            }
+        });
 
         addTransactionLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,190 +81,7 @@ public class TransactionFragment extends Fragment implements TransactionListAdap
             }
         });
 
-        Spinner categoryFilterSpinner = view.findViewById(R.id.category_filter_spinner);
-        ArrayAdapter<Category> categoryFilterAdapter = new ArrayAdapter<>(
-                getActivity(),
-                android.R.layout.simple_spinner_item,
-                buildCategoryFilterList()
-        );
-
-        categoryFilterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        categoryFilterSpinner.setAdapter(categoryFilterAdapter);
-        categoryFilterSpinner.setOnItemSelectedListener(new SpinnerListener());
-
-        // Date filter
-
-        editTextFromDate = view.findViewById(R.id.fromDate);
-        editTextToDate = view.findViewById(R.id.toDate);
-
-        DatePickerDialog.OnDateSetListener fromDateListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(
-                    final DatePicker view,
-                    final int year,
-                    final int monthOfYear,
-                    final int dayOfMonth
-            ) {
-                // TODO Auto-generated method stub
-                fromDate.set(Calendar.YEAR, year);
-                fromDate.set(Calendar.MONTH, monthOfYear);
-                fromDate.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateTransactionDateLabel(editTextFromDate, fromDate);
-            }
-
-        };
-
-        editTextFromDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                // TODO Auto-generated method stub
-                new DatePickerDialog(
-                        getContext(),
-                        fromDateListener,
-                        fromDate.get(Calendar.YEAR),
-                        fromDate.get(Calendar.MONTH),
-                        fromDate.get(Calendar.DAY_OF_MONTH)
-                ).show();
-            }
-        });
-
-        DatePickerDialog.OnDateSetListener toDateListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(
-                    final DatePicker view,
-                    final int year,
-                    final int monthOfYear,
-                    final int dayOfMonth
-            ) {
-                // TODO Auto-generated method stub
-                toDate.set(Calendar.YEAR, year);
-                toDate.set(Calendar.MONTH, monthOfYear);
-                toDate.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateTransactionDateLabel(editTextToDate, toDate);
-            }
-        };
-
-        editTextToDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                // TODO Auto-generated method stub
-                new DatePickerDialog(
-                        getContext(),
-                        toDateListener,
-                        toDate.get(Calendar.YEAR),
-                        toDate.get(Calendar.MONTH),
-                        toDate.get(Calendar.DAY_OF_MONTH)
-                ).show();
-            }
-        });
-
-        ImageButton imageButton = view.findViewById(R.id.imageDateButton);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                long fromDateInMS = parseFromDate();
-                long toDateInMS = parseToDate();
-                boolean filterByDateEnabled = !(
-                        editTextFromDate.getText().toString().equals("")
-                        && editTextToDate.getText().toString().equals("")
-                );
-
-                transactionViewModel.setDateFilter(filterByDateEnabled, fromDateInMS, toDateInMS);
-            }
-        });
-
-        // Amount filter
-
-        editTextFromAmount = view.findViewById(R.id.fromAmount);
-        editTextToAmount = view.findViewById(R.id.toAmount);
-
-        imageAmountButton = view.findViewById(R.id.imageAmountButton);
-
-        editTextFromAmount.addTextChangedListener(amountTextWatcher);
-        editTextToAmount.addTextChangedListener(amountTextWatcher);
-
-        imageAmountButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                float fromAmount = Float.parseFloat(editTextFromAmount.getText().toString());
-                float toAmount = Float.parseFloat(editTextToAmount.getText().toString());
-
-                transactionViewModel.setAmountFilter(true, fromAmount, toAmount);
-            }
-        });
-
         return view;
-    }
-
-    private TextWatcher amountTextWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(final CharSequence s, final int start, final int count, final int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(final CharSequence s, final int start, final int before, final int count) {
-
-            if (editTextFromAmount.getText().toString().isEmpty()
-                    ||
-                    editTextToAmount.getText().toString().isEmpty()) {
-                imageAmountButton.setEnabled(false);
-            } else {
-                imageAmountButton.setEnabled(true);
-            }
-        }
-
-        @Override
-        public void afterTextChanged(final Editable s) {
-
-        }
-    };
-
-    /**
-     * Method to update the transaction date with the selected date.
-     * @param editTextLayout editText transaction date
-     * @param myCalendar calendar the calendar to choose date
-     */
-    private void updateTransactionDateLabel(final EditText editTextLayout, final Calendar myCalendar) {
-        String myFormat = "dd-MM-yy"; //In which you need put here
-        editTextLayout.setText(DateUtils.formatDate(myCalendar.getTimeInMillis(), myFormat));
-    }
-
-    /**
-     * Helper method to build the category filter list and add a default "Select all" pseudo category.
-     * @return category list.
-     */
-    private List<Category> buildCategoryFilterList() {
-        List<Category> categoryFilterList = new ArrayList<>();
-
-        Category noCategory = new Category(-1, "Select all", -1);
-        List<Category> categories = categoryViewModel.getAllCategories();
-
-        categoryFilterList.add(noCategory);
-        categoryFilterList.addAll(categories);
-
-        return categoryFilterList;
-    }
-
-    private class SpinnerListener implements AdapterView.OnItemSelectedListener {
-
-        /**
-         * Default constructor.
-         */
-        SpinnerListener() { }
-
-        @Override
-        public void onItemSelected(final AdapterView<?> parent, final View view, final int position, final long id) {
-            Category selectedCategory = (Category) parent.getItemAtPosition(position);
-            boolean filterEnabled = selectedCategory.getId() != -1;
-
-            transactionViewModel.setCategoryFilter(filterEnabled, selectedCategory.getId().toString());
-        }
-
-        @Override
-        public void onNothingSelected(final AdapterView<?> parent) {
-            Log.v("SPINNER", "NOTHING SELECTED");
-        }
     }
 
     /**
@@ -292,44 +105,5 @@ public class TransactionFragment extends Fragment implements TransactionListAdap
         fragmentTransaction.replace(R.id.fragment_container, fragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
-    }
-
-    /**
-     * Parse fromDate for date filter.
-     * @return parsed from date.
-     */
-    private long parseFromDate() {
-        if (!editTextFromDate.getText().toString().equals("")) {
-            return DateUtils.getStartOfDayInMS(fromDate.getTimeInMillis());
-        } else {
-            if (!editTextToDate.getText().toString().equals("")) {
-                return DateUtils.getStartOfDayInMS(0);
-            } else {
-                return DateUtils.getStartOfCurrentDay().getTimeInMillis();
-            }
-        }
-    }
-
-    /**
-     * Parse toDate for date filter.
-     * @return parsed to date.
-     */
-    private long parseToDate() {
-        if (!editTextToDate.getText().toString().equals("")) {
-            return DateUtils.getEndOfDayInMS(toDate.getTimeInMillis());
-        } else {
-            if (!editTextFromDate.getText().toString().equals("")) {
-                if (
-                        DateUtils.getStartOfDayInMS(fromDate.getTimeInMillis())
-                                > DateUtils.getStartOfCurrentDay().getTimeInMillis()
-                ) {
-                    return DateUtils.getEndOfDayInMS(fromDate.getTimeInMillis());
-                } else {
-                    return DateUtils.getEndOfCurrentDay().getTimeInMillis();
-                }
-            } else {
-                return DateUtils.getEndOfCurrentDay().getTimeInMillis();
-            }
-        }
     }
 }
