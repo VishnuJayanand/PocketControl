80c763465d229770ec5e2b6f57bb913465a1f0ed
Lauro F. Müller <lauro.muller@lfmuller.com>
2020-06-12 10:37:47 +0200

    finish design sign in

--- app/src/main/java/com/droidlabs/pocketcontrol/ui/signin/SignInActivity.java
+++ app/src/main/java/com/droidlabs/pocketcontrol/ui/signin/SignInActivity.java
@@ -27,7 +27,8 @@ import java.util.List;
  */
 public class SignInActivity extends AppCompatActivity {
 
-    private View mContentView;
+    private View signInContainer;
+    private View signUpContainer;
     private UserViewModel userViewModel;
     private List<User> userList;
     private TextView currentUser;
@@ -38,14 +39,9 @@ public class SignInActivity extends AppCompatActivity {
     private Button validateCredentialsButton;
     private Button createNewAccountButton;
     private Button alreadyHaveAnAccountButton;
-    private Button signInWithPasswordButton;
-    private Button signInWithPinButton;
     private User user;
     private TextInputEditText fourDigitPinValue;
-    private TextInputEditText passwordSignInValue;
-    private TextInputLayout passwordInputSignIn;
     private TextInputLayout accessPinInputGroup;
-    private boolean authenticateWithPassword;
 
     /**
      * Create a new activity.
@@ -57,27 +53,19 @@ public class SignInActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_sign_in_screen);
 
-        mContentView = findViewById(R.id.signin_container);
+        signInContainer = findViewById(R.id.signinContainer);
+        signUpContainer = findViewById(R.id.signupContainer);
 
-        currentUser = mContentView.findViewById(R.id.currentUserEmail);
-        selectedUser = mContentView.findViewById(R.id.selectedUser);
-        createNewUser = mContentView.findViewById(R.id.createNewUser);
-        enterUserPin = mContentView.findViewById(R.id.enterUserPin);
-        createNewAccountButton = mContentView.findViewById(R.id.createNewAccountButton);
-        alreadyHaveAnAccountButton = mContentView.findViewById(R.id.alreadyHaveAnAccountButton);
-        signUpButton = mContentView.findViewById(R.id.signUpButton);
-        validateCredentialsButton = mContentView.findViewById(R.id.validateCredentialsButton);
+        currentUser = signInContainer.findViewById(R.id.currentUserEmail);
+        selectedUser = signInContainer.findViewById(R.id.selectedUser);
+        enterUserPin = signInContainer.findViewById(R.id.enterUserPin);
+        createNewAccountButton = signInContainer.findViewById(R.id.createNewAccountButton);
+        validateCredentialsButton = signInContainer.findViewById(R.id.validateCredentialsButton);
+        fourDigitPinValue = signInContainer.findViewById(R.id.fourDigitPinValue);
+        accessPinInputGroup = signInContainer.findViewById(R.id.accessPinInputGroup);
 
-
-        fourDigitPinValue = mContentView.findViewById(R.id.fourDigitPinValue);
-        passwordSignInValue = mContentView.findViewById(R.id.passwordSignInValue);
-
-
-        signInWithPasswordButton = mContentView.findViewById(R.id.signInWithPasswordButton);
-        signInWithPinButton = mContentView.findViewById(R.id.signInWithPinButton);
-
-        passwordInputSignIn = mContentView.findViewById(R.id.passwordInputSignInGroup);
-        accessPinInputGroup = mContentView.findViewById(R.id.accessPinInputGroup);
+        alreadyHaveAnAccountButton = signUpContainer.findViewById(R.id.alreadyHaveAnAccountButton);
+        signUpButton = signUpContainer.findViewById(R.id.signUpButton);
 
         userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         userViewModel.getAllUsers().observe(this, new Observer<List<User>>() {
@@ -87,7 +75,7 @@ public class SignInActivity extends AppCompatActivity {
 
                 if (users.size() > 0) {
                     new SharedPreferencesUtils(getApplication()).setCurrentUserId(String.valueOf(users.get(0).getId()));
-                    currentUser.setText(users.get(0).getEmail());
+                    currentUser.setText("" + users.get(0).getEmail() + users.get(0).getEmail());
                     user = userViewModel.getUserById(users.get(0).getId());
                 }
             }
@@ -107,22 +95,6 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
 
-        signInWithPasswordButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                authenticateWithPassword = true;
-                showPasswordSignIn();
-            }
-        });
-
-        signInWithPinButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                authenticateWithPassword = false;
-                showPinSignIn();
-            }
-        });
-
         validateCredentialsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
@@ -135,47 +107,21 @@ public class SignInActivity extends AppCompatActivity {
      * Execute sign in and move to home screen.
      */
     private void signIn() {
-        boolean authenticated = false;
-
-        if (authenticateWithPassword) {
-            if (passwordSignInValue.getText().toString().equals(user.getPassword())) {
-                authenticated = true;
-            }
-        } else {
-            if (fourDigitPinValue.getText().toString().equals(user.getAccessPin())) {
-                authenticated = true;
-            }
-        }
-
-        if (authenticated) {
+        if (fourDigitPinValue.getText().toString().equals(user.getAccessPin())) {
             Intent intent = new Intent(getApplication(), HomeActivity.class);
             startActivity(intent);
+        } else {
+            // TODO: show error message
         }
     }
 
     private void showSignUpContent() {
-        createNewUser.setVisibility(View.VISIBLE);
-        selectedUser.setVisibility(View.GONE);
-        enterUserPin.setVisibility(View.GONE);
+        signInContainer.setVisibility(View.GONE);
+        signUpContainer.setVisibility(View.VISIBLE);
     }
 
     private void showSignInContent() {
-        createNewUser.setVisibility(View.GONE);
-        selectedUser.setVisibility(View.VISIBLE);
-        enterUserPin.setVisibility(View.VISIBLE);
-    }
-
-    private void showPasswordSignIn() {
-        accessPinInputGroup.setVisibility(View.GONE);
-        signInWithPasswordButton.setVisibility(View.GONE);
-        signInWithPinButton.setVisibility(View.VISIBLE);
-        passwordInputSignIn.setVisibility(View.VISIBLE);
-    }
-
-    private void showPinSignIn() {
-        passwordInputSignIn.setVisibility(View.GONE);
-        signInWithPinButton.setVisibility(View.GONE);
-        signInWithPasswordButton.setVisibility(View.VISIBLE);
-        accessPinInputGroup.setVisibility(View.VISIBLE);
+        signInContainer.setVisibility(View.VISIBLE);
+        signUpContainer.setVisibility(View.GONE);
     }
 }
